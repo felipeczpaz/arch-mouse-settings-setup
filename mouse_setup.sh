230244c56f2d9fd058edb54a8875efc108c42b59
Felipe Cezar P. <131888847+felipeczpaz@users.noreply.github.com>
2025-01-23 18:01:10 -0300

    Create mouse_setup.sh

---
+++ mouse_setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Disable mouse acceleration for all mice
+for device in $(xinput --list | grep -i "mouse" | awk -F'id=' '{print $2}' | awk '{print $1}'); do
+    # Set acceleration speed to 0 (no acceleration)
+    xinput set-prop "$device" "libinput Accel Speed" 0
+    # Set the acceleration profile to "none" (if applicable)
+    if xinput list-props "$device" | grep -q "libinput Accel Profile Enabled"; then
+        xinput set-prop "$device" "libinput Accel Profile Enabled" 0, 1, 0  # Set to "none"
+    fi
+done
+
+# Enable tapping on touchpad
+for device in $(xinput --list | grep -i "touchpad" | awk -F'id=' '{print $2}' | awk '{print $1}'); do
+    # Enable tapping
+    xinput set-prop "$device" "libinput Tapping Enabled" 1
+done
+
+# Invert scroll direction for all devices
+for device in $(xinput --list | grep -i "pointer" | awk -F'id=' '{print $2}' | awk '{print $1}'); do
+    # Invert natural scrolling
+    if xinput list-props "$device" | grep -q "libinput Natural Scrolling Enabled"; then
+        xinput set-prop "$device" "libinput Natural Scrolling Enabled" 1
+    fi
+done
